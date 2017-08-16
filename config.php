43d7393e2208d82c22e6ea8f42f5a1ab24376d66
Bipin Jitiya <bipinjitiya77@gmail.com>
2017-08-16 22:42:08 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/*		
+ *	Developed By : Bipin Jitiya.
+ *	This is the Main Configuration file for News and Admin Panel.
+ *	Year : 2017
+*/
+
+//####################### CONFIG START ########################
+
+define('DB_USER', "xxx"); 
+define('DB_PASS', "xxx");
+define('DB_NAME', "xxx");
+define('DB_HOST', "localhost");
+define('AUTH_PASS', "21232f297a57a5a743894a0e4a801fc3"); // MD5 hash 'admin'
+
+//######################## CONFIG END #########################
+?>
