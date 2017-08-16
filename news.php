cdedaac7ab4c93b8cc2c4bf3f9c54a7c67363b6d
Bipin Jitiya <bipinjitiya77@gmail.com>
2017-08-16 23:00:42 +0530

    Update news.php

--- news.php
+++ news.php
@@ -1,7 +1,7 @@
 <marquee scrolldelay="200" direction="up" behavior="scroll" onMouseOver="this.stop();" onMouseOut="this.start();" style="height: 220px;">
 	<ul class="list-group">
 		<?php
-			require_once('../config.php');
+			require_once('config.php');
 			$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die(mysqli_connect_error());
 			$result = mysqli_query($con,"SELECT subject FROM news") or die(mysqli_connect_error());
 			if (mysqli_num_rows($result) > 0){
